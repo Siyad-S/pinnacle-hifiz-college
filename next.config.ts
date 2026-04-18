7e956331ffa7b477d6781ca20e490e5955ed2263
Siyad-S <siyadsaidu313@gmail.com>
2026-04-18 10:50:28 +0530

    issue with SEO

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,76 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'hifzulquaranacademy.com',
+          },
+        ],
+        destination: 'https://www.pinnaclehifzulquranacademy.com/:path*',
+        permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.hifzulquaranacademy.com',
+          },
+        ],
+        destination: 'https://www.pinnaclehifzulquranacademy.com/:path*',
+        permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'pinnaclehifzulquaranacademy.com',
+          },
+        ],
+        destination: 'https://www.pinnaclehifzulquranacademy.com/:path*',
+        permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.pinnaclehifzulquaranacademy.com',
+          },
+        ],
+        destination: 'https://www.pinnaclehifzulquranacademy.com/:path*',
+        permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.pinnacleacademy.com',
+          },
+        ],
+        destination: 'https://www.pinnaclehifzulquranacademy.com/:path*',
+        permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'pinnacleacademy.com',
+          },
+        ],
+        destination: 'https://www.pinnaclehifzulquranacademy.com/:path*',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
